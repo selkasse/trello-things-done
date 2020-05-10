84dcf069595b6a354cb993d3ad25257e9ef343fd
selkasse <selkasse@gmu.edu>
2020-05-10 19:52:42 -0400

    comment out local storage

--- public/js/client.js
+++ public/js/client.js
@@ -108,7 +108,7 @@ TrelloPowerUp.initialize({
                 enabledBoards
             };
 
-            window.localStorage.setItem('config', JSON.stringify(configParams));
+            // window.localStorage.setItem('config', JSON.stringify(configParams));
 
             return [{
                 icon: {
