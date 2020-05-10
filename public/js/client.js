2381f298fc90ff2cf427f5dc029cf08183e0b5b3
selkasse <selkasse@gmu.edu>
2020-05-09 20:31:28 -0400

    use larger icons for master icon

--- public/js/client.js
+++ public/js/client.js
@@ -1,8 +1,8 @@
 const Promise = TrelloPowerUp.Promise;
 
 const CHECK_MARK_ICON = 'https://img.icons8.com/material/24/000000/check-all.png';
-const MASTER_ICON_DARK = 'https://img.icons8.com/material/24/000000/master.png';
-const MASTER_ICON_LIGHT = 'https://img.icons8.com/material-outlined/24/000000/master.png';
+const MASTER_ICON_DARK = 'https://img.icons8.com/material/48/000000/master.png';
+const MASTER_ICON_LIGHT = 'https://img.icons8.com/material-outlined/48/000000/master.png';
 
 
 const fetchID = async () => {
