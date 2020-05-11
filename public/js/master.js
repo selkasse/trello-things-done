c89e21c6c8b512edcd0a21d3259ce496caa1ca45
selkasse <selkasse@gmu.edu>
2020-05-11 18:54:52 -0400

    uncomment localstorage reference in master.js

--- public/js/master.js
+++ public/js/master.js
@@ -11,8 +11,8 @@ window.master.addEventListener('submit', function (event) {
         });
 });
 
-// const config = JSON.parse(window.localStorage.getItem('config'));
-// const boards = config.enabledBoards;
+const config = JSON.parse(window.localStorage.getItem('config'));
+const boards = config.enabledBoards;
 
 // * add the boards to the dropdown when master.html renders
 t.render(async function () {
