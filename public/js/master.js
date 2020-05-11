7e398f21dc9c8aa3a3745c1f00b6b767750eb0b0
selkasse <selkasse@gmu.edu>
2020-05-10 22:58:32 -0400

    comment out client side api calls

--- public/js/master.js
+++ public/js/master.js
@@ -12,41 +12,41 @@ window.master.addEventListener('submit', function (event) {
         });
 });
 
-// get the member (user) ID
-async function getMemberId() {
+// // get the member (user) ID
+// async function getMemberId() {
 
-    let response = await fetch('https://api.trello.com/1/members/me?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661');
-    let memberResponse = await response.json();
-    return memberResponse.id;
+//     let response = await fetch('https://api.trello.com/1/members/me?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661');
+//     let memberResponse = await response.json();
+//     return memberResponse.id;
 
-}
+// }
 
-// get the boards that belong to the user
-async function getMemberBoards() {
-    const memberId = await getMemberId();
+// // get the boards that belong to the user
+// async function getMemberBoards() {
+//     const memberId = await getMemberId();
 
-    let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
-    let boardsJSON = await boardsResponse.json();
-    let boards = [];
-    for (board in boardsJSON) {
-        const obj = {
-            id: boardsJSON[board].id,
-            name: boardsJSON[board].name
-        };
-        boards.push(obj);
-    }
-    return boards;
-}
+//     let boardsResponse = await fetch(`https://api.trello.com/1/members/${memberId}/boards?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`);
+//     let boardsJSON = await boardsResponse.json();
+//     let boards = [];
+//     for (board in boardsJSON) {
+//         const obj = {
+//             id: boardsJSON[board].id,
+//             name: boardsJSON[board].name
+//         };
+//         boards.push(obj);
+//     }
+//     return boards;
+// }
 
-async function checkIfEnabled(id) {
-    let response = await fetch(`https://api.trello.com/1/boards/${id}/boardPlugins?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`)
-    let powerupsJSON = await response.json();
-    for (powerup in powerupsJSON) {
-        // TODO: put the powerup ID in a separate file
-        if (powerupsJSON[powerup].idPlugin === '5eb0ba3ec071f670fcd8b0dd') return true;
-    }
-    return false;
-}
+// async function checkIfEnabled(id) {
+//     let response = await fetch(`https://api.trello.com/1/boards/${id}/boardPlugins?key=41ae5bff41af5eac3f32ad7a4daab49e&token=5e71d684035b882896f8ecfc32de15dee8c64b0e73b8c965609c3c7473f47661`)
+//     let powerupsJSON = await response.json();
+//     for (powerup in powerupsJSON) {
+//         // TODO: put the powerup ID in a separate file
+//         if (powerupsJSON[powerup].idPlugin === '5eb0ba3ec071f670fcd8b0dd') return true;
+//     }
+//     return false;
+// }
 
 
 // add the boards to the dropdown when master.html renders
