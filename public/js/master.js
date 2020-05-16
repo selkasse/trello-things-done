0c8c66348b0b770b1a0914eeae46f2d9599796ae
selkasse <selkasse@gmu.edu>
2020-05-16 14:56:36 -0400

    use t.get instead of localStorage in master.js

--- public/js/master.js
+++ public/js/master.js
@@ -11,12 +11,19 @@ window.master.addEventListener('submit', function(event) {
     });
 });
 
-const config = JSON.parse(window.localStorage.getItem('config'));
-const boards = config.enabledBoards;
+// const config = JSON.parse(window.localStorage.getItem('config'));
 
 // * add the boards to the dropdown when master.html renders
 t.render(async function() {
     const masterBoard = await t.get('member', 'shared', 'masterBoard');
+    let boards;
+
+    try {
+        const configResponse = await t.get('organization', 'shared', 'config', 'not set');
+        boards = JSON.stringify(configResponse);
+    } catch (e) {
+        console.log(e);
+    }
     const select = document.getElementById('masterBoard');
     for (let i = 0; i < boards.length; i += 1) {
         console.log(boards[i]);
