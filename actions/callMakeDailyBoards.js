93451d2b9cc84c53f6bf42dc561731ffa00f058f
selkasse <selkasse@gmu.edu>
2020-05-17 17:56:35 -0400

    install actions toolkit

--- actions/callMakeDailyBoards.js
+++ actions/callMakeDailyBoards.js
@@ -3,8 +3,12 @@ const axios = require('axios');
 const { TRELLO_MEMBER } = process.env;
 
 const callNetlify = async () => {
-    const scheduleResponse = await axios.post('/.netlify/functions/makeDailyBoards', { memberID: TRELLO_MEMBER });
-    console.log(scheduleResponse);
+    try {
+        const scheduleResponse = await axios.post('/.netlify/functions/makeDailyBoards', { memberID: TRELLO_MEMBER });
+        console.log(scheduleResponse);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 callNetlify();
