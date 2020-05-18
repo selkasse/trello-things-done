7beecb26d645918a14d7f655e0ed77d5b42486be
selkasse <selkasse@gmu.edu>
2020-05-18 08:45:41 -0400

    add server.js

---
+++ .github/actions/daily-action/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// * your manifest must have appropriate CORS headers, you could also use '*'
+app.use(cors({ origin: 'https://netlify.com' }));
+
+// * http://expressjs.com/en/starter/static-files.html
+app.use(express.static('dist'));
+
+// * http://expressjs.com/en/starter/basic-routing.html
+app.get('*', function (request, response) {
+    response.sendFile(`${__dirname}/index.js`);
+});
+
+// * listen for requests :)
+const listener = app.listen(80, function () {
+    console.log(`Your app is listening on port 80!`);
+});
