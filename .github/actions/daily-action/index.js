45daae5de5f3e0cf257df10cc5a97b1beaa89eaf
selkasse <selkasse@gmu.edu>
2020-05-18 21:03:25 -0400

    add server.js code to index.js

--- .github/actions/daily-action/index.js
+++ .github/actions/daily-action/index.js
@@ -4,10 +4,32 @@
     //* https://help.github.com/en/actions/building-actions/creating-a-javascript-action
 */
 const axios = require('axios');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+const express = require('express');
+const cors = require('cors');
+
 
 
 // const { TRELLO_MEMBER } = process.env;
+const setup = () => {
+    const app = express();
+
+    // * your manifest must have appropriate CORS headers, you could also use '*'
+    app.use(cors({ origin: 'https://netlify.com' }));
+
+    // * http://expressjs.com/en/starter/static-files.html
+    app.use(express.static('dist'));
+
+    // * http://expressjs.com/en/starter/basic-routing.html
+    app.get('*', function (request, response) {
+        response.sendFile(`${__dirname}/index.js`);
+    });
+
+    // * listen for requests :)
+    const listener = app.listen(8000, function () {
+        console.log(`Your app is listening on port ${listener.address().port}`);
+    });
+}
 const callNetlify = async () => {
 
     try {
@@ -20,5 +42,6 @@ const callNetlify = async () => {
     }
 };
 
+setup();
 callNetlify();
 
