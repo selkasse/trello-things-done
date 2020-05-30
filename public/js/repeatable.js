c699827754f06290cae17a8aff25c595deb5159b
selkasse <selkasse@gmu.edu>
2020-05-29 20:50:07 -0400

    move label inside daily form div

--- public/js/repeatable.js
+++ public/js/repeatable.js
@@ -45,8 +45,6 @@ window.repeatable.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const error = document.getElementById('errorLabel');
-    error.classList.remove('error');
-    error.classList.add('hidden');
 
     if (window.repeatSchedule.value === 'selectDays') {
         const mondays = document.getElementById('mondays').checked;
@@ -61,6 +59,8 @@ window.repeatable.addEventListener('submit', function(event) {
         const daysArray = Object.values(days);
         const oneBoxChecked = daysArray.find(checked => checked === true);
         if (oneBoxChecked) {
+            error.classList.remove('error');
+            error.classList.add('hidden');
             console.log('at least one day was selected');
         } else {
             error.classList.remove('hidden');
